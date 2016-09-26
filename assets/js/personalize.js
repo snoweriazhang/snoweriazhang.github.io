813ed35c026c070cdf9d9f38d53e2a29ba111091
snoweriazhang <dobbyzhang@gmail.com>
2016-09-25 23:57:33 -0400

    added parallax to home page

--- assets/js/personalize.js
+++ assets/js/personalize.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 	$(window).scroll(function(){
 		$top = $(window).scrollTop();
-		$('#aboutwrap').css('-webkit-transform', 'translateY('+ ($top/2) +'px)');
-		$('#aboutwrap').css('-moz-transform', 'translateY('+ ($top/2) +'px)');
-		$('#aboutwrap').css('-ms-transform', 'translateY('+ ($top/2) +'px)');
-		// $closeToBottom = ($top + $(window).height() > $(document).height() - 100);
-	 //    	if($closeToBottom) {
-	 //      		$('#parallax_slow').after($('canvas').clone());
-	 //    	}
+
+    $('#headerwrap').css('-webkit-transform', 'translateY('+ ($top/2.5) +'px)');
+		$('#headerwrap').css('-moz-transform', 'translateY('+ ($top/2.5) +'px)');
+		$('#headerwrap').css('-ms-transform', 'translateY('+ ($top/2.5) +'px)');
+
+		$('#aboutwrap').css('-webkit-transform', 'translateY('+ ($top/2.5) +'px)');
+		$('#aboutwrap').css('-moz-transform', 'translateY('+ ($top/2.5) +'px)');
+		$('#aboutwrap').css('-ms-transform', 'translateY('+ ($top/2.5) +'px)');
 	});
 });
